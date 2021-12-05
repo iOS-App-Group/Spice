c212ac5931d5f0d590013bd0fea940389b50c2e5
Salome Asatiani <61980344+Salteki@users.noreply.github.com>
2021-12-05 13:43:53 -0500

    added code for searchcontroller

---
+++ Spice/SearchController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchController.swift
+//  Spice
+//
+//  Created by Salome Asatiani on 12/5/21.
+//
+
+import Foundation
+import UIKit
+
+class SearchController: UISearchController{
+    let searchCOntroller = UISearchController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Search"
+    }
+}
